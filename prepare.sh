240e3f3ae12d7d02134b0b682d51e886291d4a0a
dmitrime <dmitrime@github.com>
2016-06-03 15:53:57 +0700

    touch dirs

--- prepare.sh
+++ prepare.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
 ORIGINAL_POS_DIR=original
+POSITIVE_DIR=positive_images
+NEGATIVE_DIR=negative_images
 SAMPLES=1500
 IMG_SIZE=32
 OPENCV_SOURCE_PATh=./opencv-2.4.10/apps/haartraining
 
+
 ./bin/resize.py $ORIGINAL_POSS_DIR $IMG_SIZE
 
-find ./positive_images -iname "*.jpg" > positives.txt
-find ./negative_images -iname "*.jpg" > negatives.txt
+touch $POSITIVE_DIR
+touch $NEGATIVE_DIR
+
+find ./$POSITIVE_DIR -iname "*.jpg" > positives.txt
+find ./$NEGATIVE_DIR -iname "*.jpg" > negatives.txt
 
 ./bin/createsamples.pl positives.txt negatives.txt samples $SAMPLES  "opencv_createsamples -bgcolor 0 -bgthresh 0 -maxxangle 1.1 -maxyangle 1.1 maxzangle 0.5 -maxidev 40 -w $IMG_SIZE -h $IMG_SIZE"
 
